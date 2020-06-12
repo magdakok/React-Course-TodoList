c8ad0ed67bf8b29c8e9b68b5273a8297b4f60b32
magdakok <61601293+magdakok@users.noreply.github.com>
2020-06-12 11:05:29 +0100

    add initial state and render Todo's in TodoList component

--- src/TodoList.js
+++ src/TodoList.js
@@ -3,12 +3,22 @@ import Todo from "./Todo";
 import TodoForm from "./TodoForm";
 
 class TodoList extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            todos: [
+                {text: 'buy bread', isDone: 'false', id: 1}
+            ]
+        }
+    }
     render(){
         return (
             <div className="TodoList">
                 <h1>TodoList</h1>
-                <Todo />
                 <TodoForm />
+                {this.state.todos.map(todo =>(
+                    <Todo text={todo.text} isDone={todo.isDone} key={todo.id} />
+                ))}
             </div>
         )
     }
