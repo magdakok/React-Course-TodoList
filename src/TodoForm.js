b8bd8aee810af6c49dde2ba216bd4e8ce552d56c
magdakok <61601293+magdakok@users.noreply.github.com>
2020-06-12 11:13:42 +0100

    create TodoForm component

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,10 +1,32 @@
 import React, {Component} from "react";
 
 class TodoForm extends Component {
+    constructor(props){
+        super(props);
+        this.state = { todo: "" };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e){
+        this.setState({
+            [e.target.name] : e.target.value
+        })
+    }
+
+    handleSubmit(e){
+        e.preventDefault();
+        this.props.addTodo(this.state);
+        this.setState({ todo: "" });
+    }
+
     render(){
         return (
             <div className="TodoForm">
-                <h2>TodoForm</h2>
+                <form className="Todo__form" onSubmit={this.handleSubmit}>
+                    <input name="todo" value={this.state.todo} onChange={this.handleChange} />
+                    <button>ADD</button>
+                </form>
             </div>
         )
     }
