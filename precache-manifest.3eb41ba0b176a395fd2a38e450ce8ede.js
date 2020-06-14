db00225830e437e4f12fa72d901a1ef8a0f6f136
magdakok <61601293+magdakok@users.noreply.github.com>
2020-06-14 18:03:00 +0100

    Updates

---
+++ precache-manifest.3eb41ba0b176a395fd2a38e450ce8ede.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24fd16615477109fd6c52eba350a5f74",
+    "url": "/React-Course-TodoList/index.html"
+  },
+  {
+    "revision": "545f0426d1ec29203390",
+    "url": "/React-Course-TodoList/static/css/main.8ad44b0a.chunk.css"
+  },
+  {
+    "revision": "304e0412af614e8fbd46",
+    "url": "/React-Course-TodoList/static/js/2.a1c7632f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Course-TodoList/static/js/2.a1c7632f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "545f0426d1ec29203390",
+    "url": "/React-Course-TodoList/static/js/main.86538170.chunk.js"
+  },
+  {
+    "revision": "2106023f5fa1f4d5de19",
+    "url": "/React-Course-TodoList/static/js/runtime-main.a1e88d96.js"
+  }
+]);
